c39d40b85ee5a0ffbbf6f11724fcec6f7f211884
Jelena Kondić <jelena.kondic@stormerit.rs>
2024-11-10 23:35:33 +0100

    Dodate liste oglasa i kategorija na početnu stranicu sa dvokolonskim rasporedom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,26 @@
 @section('main')
     <h1>All ads</h1>
     <!-- Dodaj ovde kod za prikazivanje oglasa -->
+        <div class="row">
+            <div class="col-3 bg-secondary">
+                <ul class="list-group list-group-flush">
+                    @foreach ($categories as $cat)
+                        <li class="list-group-item bg-secondary">
+                                <a href="" class="text-light">{{$cat->name}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="col-9">
+                <ul class="list-group">
+                    @foreach ($all_ads as $ad)
+                        <li class="list-group-item">
+                                <a href="" >{{$ad->title}}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
 @endsection
