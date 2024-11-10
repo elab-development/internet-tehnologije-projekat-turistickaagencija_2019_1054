f562a0bfb188d4fcd4437725c20e8b6059709c6f
Jelena Kondić <jelena.kondic@stormerit.rs>
2024-11-10 21:55:12 +0100

    Dodat navbar.blade.php sa linkovima za 'Home,' 'Login,' i 'Register' koji se prikazuju prema autentikaciji korisnika. Izmenjene master.blade.php i app.blade.php za bolju organizaciju izgleda stranica.

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ad;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdController extends Controller
 {
-    //
+  public function index(){
+
+        $all_ads = Ad::all();
+        $categories = Category::all();
+        return view('welcome',compact('all_ads','categories'));
+
+   
+
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',  [App\Http\Controllers\AdController::class, 'index'])->name('welcome');
 
 Auth::routes();
 
